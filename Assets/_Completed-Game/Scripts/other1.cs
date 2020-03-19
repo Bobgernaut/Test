cf958f874a58b0e609c5d9487efa0c346da1ccb2
zigie_inc <zigie_inc@hotmail.com>
2020-03-18 23:08:00 -0400

    Test 03

---
+++ Assets/_Completed-Game/Scripts/other1.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+	/// <summary>
+	///	 
+	/// </summary>
+
+public class other1 : MonoBehaviour
+{
+	//**** Variables
+
+}
